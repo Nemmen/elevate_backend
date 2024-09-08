b1950666649fc85e034d1e195a501cc3f797679b
SuperiorSU <sujalsinha2001@gmail.com>
2024-09-08 08:56:50 +0530

    backend-closed

--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,14 @@ const {addmanagement, allmanagement} = require("../controller/management");
 const router = express.Router();
 
 
-router.post("/student", addstudent);
+// router.post("/student", addstudent);
 // all student
 router.get("/allStudents", allstudent);
 // all faculty
 router.get("/allFaculties", allfaculty);
-router.post("/faculty", addfaculty);
+// router.post("/faculty", addfaculty);
 
-router.post("/management", addmanagement);
+// router.post("/management", addmanagement);
 // all management
 router.get("/allManagement", allmanagement);
 
